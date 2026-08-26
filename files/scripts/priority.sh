f5e082a5515c28919f307f5ef51a263cbab7f912
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-08-25 21:10:09 -0500

    Added repos, priorities and packages
    Set repo priorities in priorities.sh, added all repositories in recipe.yml and added packages to install.

---
+++ files/scripts/priority.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+dnf5 config-manager setopt \
+    copr:copr.fedorainfracloud.org:ublue-os:packages.priority=109 \
+    copr:copr.fedorainfracloud.org:ublue-os:staging.priority=108 \
+    fedora.priority=99 \
+    updates.priority=98 \
+    fedora-cisco-openh264.priority=97 \
+    rpmfusion-free.priority=89 \
+    rpmfusion-nonfree.priority=88 \
+    rpmfusion-free-updates.priority=87 \
+    rpmfusion-nonfree-updates.priority=86 \
+    terra.priority=79 \
+    terra-extras.priority=78 \
+    terra-mesa.priority=77
