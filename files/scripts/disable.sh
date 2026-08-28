9c56998b709ec3b2540d5ca47632e7d61a6412ae
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-08-28 13:27:09 -0500

    User settings
    I think this is all the default user setting I want on the base image.

--- files/scripts/disable.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-dnf5 config-manager setopt '*.enabled=0'
--- files/scripts/plymouth.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-plymouth-set-default-theme bgrt-mac-style
