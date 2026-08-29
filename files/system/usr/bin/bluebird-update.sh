e44bc3d212e5c02f9881d579dc7c17aa7bcd702a
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-08-28 20:17:34 -0500

    Cleanup

--- files/system/usr/bin/bluebird-update.sh
+++ files/system/usr/bin/bluebird-update.sh
@@ -3,6 +3,13 @@
 echo "Checking for updates from bluebird..."
 rpm-ostree upgrade
 
-if rpm-ostree status --json | jq -e '.deployments[] | select(.staged == true)' > /dev/null; then
+if rpm-ostree status --json | jq -e '.deployments[] | select(.staged == true)' >/dev/null; then
+    echo
     echo "Restart this system to install updates..."
+else
+    echo
+    echo "No updates available..."
 fi
+
+echo
+read -r -p "Press Enter to close..."
