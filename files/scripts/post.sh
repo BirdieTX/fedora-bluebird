98c13cdc361719afa5e091a60349c408cdcc9088
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-09-07 11:24:35 -0500

    New recipe layout

--- files/scripts/post.sh
+++ files/scripts/post.sh
@@ -2,8 +2,10 @@
 
 set -oue pipefail
 
-dnf5 config-manager setopt '*.enabled=0'
 dconf update
 systemctl disable NetworkManager-wait-online.service
 systemctl enable grub-config.service
+systemctl enable bluebird-pin-deployment.timer
+dnf5 upgrade --allow-vendor-change --refresh -y
+dnf5 config-manager setopt '*.enabled=0'
 plymouth-set-default-theme bgrt-mac-style
--- files/scripts/priority.sh
+++ files/scripts/priority.sh
@@ -4,9 +4,22 @@ set -oue pipefail
 
 dnf5 config-manager setopt \
     copr:copr.fedorainfracloud.org:ublue-os:packages.priority=109 \
+    copr:copr.fedorainfracloud.org:ublue-os:akmods.priority=108 \
+    updates-archive.priority=100 \
     fedora.priority=99 \
     updates.priority=98 \
     fedora-cisco-openh264.priority=97 \
     terra.priority=89 \
     fedora-multimedia.priority=88 \
     terra-mesa.priority=87
+
+dnf5 config-manager setopt \
+    copr:copr.fedorainfracloud.org:ublue-os:packages.name='Universal Blue' \
+    copr:copr.fedorainfracloud.org:ublue-os:akmods.name='Nvidia Drivers' \
+    fedora.name='Fedora Core' \
+    fedora-cisco-openh264.name='Cisco OpenH264' \
+    fedora-multimedia.name='Fedora Multilib' \
+    terra.name='Fyra Labs Terra' \
+    terra-mesa.name='Fyra Labs Mesa' \
+    updates.name='Fedora Updates' \
+    updates-archive.name='Fedora Updates (Archive)'
