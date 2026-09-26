119778c0872ce4df80fa442041a13ac1f1a901b4
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-09-25 22:04:59 -0500

    Fish + Extensions
    Added extensions to deck build and fixed fish config issues, so added it into skel.

---
+++ files/bluebird/etc/skel/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+	# Commands to run in interactive sessions can go here
+
+	# Remove fish greeting
+	set -U fish_greeting
+
+	# Run fastfetch configuration on shell startup
+	fastfetch -c ~/etc/fastfetch/term.jsonc
+end
+
+starship init fish | source
